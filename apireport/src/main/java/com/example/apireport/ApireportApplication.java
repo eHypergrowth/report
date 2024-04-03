b28e0e1f6860855c4a4b60eec6a1edf622cf0175
root <root@emmanuelTesting.report.diagnocons.com>
2024-04-03 16:28:22 -0700

    Primer commit

---
+++ apireport/src/main/java/com/example/apireport/ApireportApplication.java
@@ -0,0 +1,13 @@
+package com.example.apireport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApireportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApireportApplication.class, args);
+	}
+
+}
---
+++ apireport/src/test/java/com/example/apireport/ApireportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apireport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApireportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
